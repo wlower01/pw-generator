701458b59a9c116d79136b14ced9cc1df9a1b306
wlower01 <william.lower2477@gmail.com>
2021-10-30 23:03:30 -0500

    added character arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,23 @@
 // Assignment code here
+var generateBtn = document.querySelector("#generate");
 
-//function to set password length
+generateBtn.addEventListener("click", writePassword);
+
+
 var passwordLength = function() {
   var length = "";
 
-  while (length === "" || length === ) {
+  while (length === "" || length === null) {
     length = prompt("How long would you like your password? Please insert a number between 8 and 128 characters.");
   }
-  
 }
 
-var randomLowercase = ();
-
-var randomUppercase = ();
-
-var randomNumeric = ();
-
-var randomSpecialCharacters = ();
+var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numericalCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var specialCharacters = ["!", " ", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "=", "?", "@", "[", "]", "\'", "_", "^", "`", "{", "}", "|", "~"]
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
@@ -31,4 +29,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+

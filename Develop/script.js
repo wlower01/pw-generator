8b6455008a69c896a67c9771cca6ec005a2f6af9
wlower01 <william.lower2477@gmail.com>
2021-10-31 02:03:02 -0500

    trial and error for working out how to apple desired character length.

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,10 @@
 // Assignment code here
 var generateBtn = document.querySelector("#generate");
 //character variables
-var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numericalCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var specialCharacters = ["!", " ", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "=", "?", "@", "[", "]", "_", "^", "`", "{", "}", "|", "~"]
+const lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+const numericalCharacters = "0123456789"; 
+const specialCharacters = "`~!@#$%&^*()_-+=|{[}]:;<,>.?/"; 
 
 // Get references to the #generate element
 function generatePassword() {
@@ -15,17 +15,21 @@ function generatePassword() {
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  //length selection 
+//length selection 
   var passwordLength = prompt("How long would you like your password? Please insert a number between 8 and 128 characters.");
+    
+    for (var i = 0; i < passwordLength; i++ ) {
+      "";
+    }
     passwordLength = parseInt(passwordLength);
-
+//passwordLength = passwordLength + password
     if (passwordLength < 8) {
       alert("Password must be at least 8 characters long.");
       return "";
     }
 
     if (passwordLength > 128) {
-      alert("Password must be less than 129 characters long.");
+      alert("Password must be no more than 128 characters long.");
       return "";
     }
 //lowercase character confirmation yes or no
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Password Generator Starter Code
+
+For this project, the goal was to create a password generator that could provide unique and strong passwords, as well as recieve input that directs and controls what the end result looks like.
